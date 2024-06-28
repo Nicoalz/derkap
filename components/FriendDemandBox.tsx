05667b26e1127f49a47c010d5a42e1dd1be49d51
Thomas Aubert <90507019+N0rooo@users.noreply.github.com>
2024-06-28 15:12:38 +0200

    :bug: Fix error src null

--- components/FriendDemandBox.tsx
+++ components/FriendDemandBox.tsx
@@ -12,7 +12,7 @@ export default function FriendDemandBox({ demand, className, ...props }: FriendD
   return (
     <div {...props} className={cn("rounded-xl gap-x-4 items-center justify-between flex bg-white w-full p-2", className)}>
       <div className='flex items-center gap-x-4'>
-        <Image src={avatar_url} alt={username} width={60} height={60} className="rounded-xl" />
+        <Image src={avatar_url ?? ""} alt={username ?? ""} width={60} height={60} className="rounded-xl" />
 
         <div className='flex flex-col'>
           <p className='text-lg font-[600]'>{name}</p>

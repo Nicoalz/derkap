3564e2b20a583cdb1f40eaa043d27dedbf88ddbe
Lorenzo <lorenzo.aversano@edu.devinci.fr>
2024-06-28 10:28:32 +0200

    🔥 Delete useless color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,7 +17,6 @@ const config: Config = {
       },
       colors: {
         "custom-primary": "#9747ff",
-        "custom-dark": "#0a0e15",
         "custom-white": "#f5f5f5",
         "custom-black": "#1B0A32",
         "custom-violet": "#9747FF",

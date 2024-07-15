a0719e23fe5c285187811fbe6247fc0aeaab3f5a
Lorenzo <lorenzo.aversano@edu.devinci.fr>
2024-07-15 16:27:42 +0200

    :sparkles: Add details for Kaps

--- libs/mockedChallenges.ts
+++ libs/mockedChallenges.ts
@@ -1,26 +1,27 @@
-import { TChallenge } from '../types'
+import { TChallenge } from '../types';
 export const mockedChallenges: TChallenge[] = [
-    {
-      id: 1,
-      title: 'Chauve qui peut ! ',
-      subtitle: 'DERKAP du jour',
-      description: 'Prends une photo de toi avec une personne chauve !',
-      emoji: '👨‍🦲'
-    },
-    {
-      id: 2,
-      title: 'Tema la Bête',
-      subtitle: 'La folie des Road Trips',
-      description: 'Filme un animal complètement débile rencontré sur la route !',
-      emoji: '😹'
-    },
-
-    {
-      id: 3,
-      title: "¡ Flag Fiesta ! ",
-      subtitle: 'Les Français en Espagne',
-      description: 'Prends une photo de toi avec un drapeau espagnol !',
-      emoji: '🇪🇸'
-
-    }
-  ]
+  {
+    id: 1,
+    title: 'Chauve qui peut !',
+    subtitle: 'DERKAP du jour',
+    description: 'Prends une photo de toi avec une personne chauve !',
+    emoji: '👨‍🦲',
+    kapsId: 'kaps0'
+  },
+  {
+    id: 2,
+    title: 'Tema la Bête',
+    subtitle: 'La folie des Road Trips',
+    description: 'Filme un animal complètement débile rencontré sur la route !',
+    emoji: '😹',
+    kapsId: 'kaps2'
+  },
+  {
+    id: 3,
+    title: "¡ Flag Fiesta !",
+    subtitle: 'Les Français en Espagne',
+    description: 'Prends une photo de toi avec un drapeau espagnol !',
+    emoji: '🇪🇸',
+    kapsId: 'kaps1'
+  }
+];

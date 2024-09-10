f39529f8bf2edede3d36e2a8412e26e373b8a173
Thomas Aubert <90507019+N0rooo@users.noreply.github.com>
2024-09-10 15:37:24 +0200

    :bug:

--- components/form/Input.tsx
+++ components/form/Input.tsx
@@ -20,7 +20,7 @@ export default function Input({ placeholder, type, errorMsg, className, ...props
       <div className='relative'>
         <input {...props} type={inputType}
           placeholder={placeholder}
-          className={cn(" rounded-[10px] text-sm pl-[18px] text-black pr-[18px]  border-[1px] w-full border-transparent focus:outline-none shadow-base py-[12px]", className, { "border-red-500": errorMsg }, { "pr-[50px]": type === "password" })} />
+          className={cn(" bg-white rounded-[10px] text-sm pl-[18px] text-black pr-[18px]  border-[1px] w-full border-transparent focus:outline-none shadow-base py-[12px]", className, { "border-red-500": errorMsg }, { "pr-[50px]": type === "password" })} />
         {type === "password" && inputType === "password" && <Eye className=" h-[20px] w-[20px] text-gray-600 cursor-pointer absolute top-[50%] -translate-y-[50%] right-[18px]" onClick={handleShowPassword} />}
         {type === "password" && inputType === "text" && <EyeOff className=" h-[20px] w-[20px] text-gray-600  cursor-pointer absolute top-[50%] -translate-y-[50%] right-[18px] " onClick={handleShowPassword} />}
       </div>

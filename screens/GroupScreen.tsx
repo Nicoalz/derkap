56796e18df42a5563fc5a3ec537a0be2a21b7a5f
Augustin Briolon <augustin.briolon@fortuneo.com>
2024-09-13 19:27:16 +0200

    Improve UI

--- screens/GroupScreen.tsx
+++ screens/GroupScreen.tsx
@@ -1,29 +1,22 @@
 'use client';
 
-import React from "react";
+import React, { useState } from "react";
 import Header from "@/components/Header";
 import { cn } from '../lib/utils';
+import Button from "@/components/Button";
 
 const GroupScreen = ({ id }: { id: string }) => {
+  const [challengeStatus, setChallengeStatus] = useState<'posting' | 'voting' | 'ended'>('posting');
+  const [isChallenge, setIsChallenge] = useState<boolean>(false);
+  const totalElements = 15;
+  const limitElements = 8;
 
-  const imgObj: { [key: string]: { src: string; id: string, name: string } } = {
-    img1: {
-      src: 'https://picsum.photos/100',
-      id: '1',
-      name: 'img2zdzdzdqzlkfghseiufgh',
-    },
-    img2: {
-      src: 'https://picsum.photos/200',
-      id: '2',
-      name: 'img2zdzdzdqzlkfghseiufgh',
-    },
-    img3: {
-      src: 'https://picsum.photos/300',
-      id: '3',
-      name: 'img3',
-    },
+  const statusColorMap: { [key in 'posting' | 'voting' | 'ended']: string } = {
+    posting: 'bg-orange-500',
+    voting: 'bg-yellow-400',
+    ended: 'bg-gray-500',
   };
-  const challengeStatus: 'posting' | 'voting' | 'ended' = 'posting';
+
 
   const status = () => {
     if (challengeStatus === 'posting') {
@@ -33,32 +26,56 @@ const GroupScreen = ({ id }: { id: string }) => {
     } else if (challengeStatus === 'ended') {
       return 'Terminé';
     }
-  }
+  };
 
   return (
-    <div>
+    <div className="h-screen">
       <Header title='Groupe Name'>
-        <p className={cn('text-white py-1 px-2 rounded-xl text-sm', challengeStatus === 'posting' && 'bg-orange-500', challengeStatus === 'voting' && "bg-yellow-400", challengeStatus === "ended" && "bg-gray-500" )}>{status()}</p>
+        <p className={cn('text-white py-1 px-2 rounded-xl text-sm', statusColorMap[challengeStatus])}>
+          {status()}
+        </p>
       </Header>
 
-      <div className='w-full flex items-center justify-center'>
-        {Object.keys(imgObj).map((key, index) => (
-          <div className='flex flex-col items-center '>
-            <img
-              key={imgObj[key].id}
-              src={imgObj[key].src}
-              className={`min-w-10 min-h-10 max-h-10 max-w-10 rounded-full ${index !== 0 && '-ml-2'}`}
-            />
-            <p className='text-[10px] text-wrap overflow-hidden text-ellipsis max-w-8'>{imgObj[key].name}</p>
+      <div className='w-full flex items-start justify-center'>
+        {Array.from({ length: totalElements })
+          .slice(0, limitElements)
+          .map((_, index) => (
+            <div className={`flex flex-col items-center ${index !== 0 && '-ml-2'}`} style={{ zIndex: totalElements - index }} key={index}>
+              <img
+                src={`https://picsum.photos/${index}00`}
+                className={`min-w-10 min-h-10 max-h-10 max-w-10 rounded-full`}
+              />
+              {/* <p className='text-[10px] text-wrap overflow-hidden text-ellipsis max-w-8'>img {index + 1}</p> */}
+            </div>
+          ))}
+
+        {Array.from({ length: totalElements }).length > limitElements && (
+          <div className='flex flex-col items-center'>
+            <div className="w-10 h-10 border border-custom-black rounded-full -ml-2 flex items-center justify-center">
+              <p className="text-lg text-custom-black">+{totalElements - limitElements}</p>
+            </div>
           </div>
-        ))}
+        )}
       </div>
 
 
+      <div className='w-full h-[80%] flex flex-col items-center justify-between'>
+      {
+        isChallenge ? (
+          <>
+          
+          </>
+        ) : (
+          <>
+            <p>Pas de challenge pour le moment... <br /> Lancer le premier dès maintenant !</p>
+            <Button text="+" onClick={() => setIsChallenge(true)} />
+          </>
+        )
+      }
+      </div>
 
     </div>
   );
 }
-
 
 export default GroupScreen;

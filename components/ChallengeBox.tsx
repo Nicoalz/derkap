543bfb65f47efacb8922609e3a930ed318675e05
Augustin Briolon <augustin.briolon@fortuneo.com>
2024-10-10 23:55:02 +0200

    :lipstick: fix skeleton for challengebox

--- components/ChallengeBox.tsx
+++ components/ChallengeBox.tsx
@@ -20,16 +20,20 @@ const ChallengeBox = ({
         <p>Loading ...</p>
       ) : (
         <div className="min-h-16 max-h-fit flex w-full px-4 bg-custom-white border border-custom-black rounded-xl py-2 text-custom-black shadow-element gap-4 items-center">
-          <p className="text-3xl text-center">{challenge ? '😹' : '🥱'}</p>
-          <div className="text-left">
+          <div className="w-10 h-10 aspect-square flex items-center justify-center">
+            <p className="text-3xl text-center">{challenge ? '😹' : '🥱'}</p>
+          </div>
+          <div className="w-full flex flex-col gap-1">
             <p className="text-sm font-champ text-custom-black line-clamp-2 ">
               {challenge ? challenge.description : 'Pas de défi...'}
             </p>
-            <p className="text-sm line-clamp-1">
-              {challenge
-                ? 'Par ' + challenge.creator?.username
-                : 'Créez en un dès maintenant !'}
-            </p>
+            <div className="flex items-center gap-1">
+              <p className="text-sm line-clamp-1">
+                {challenge
+                  ? 'Par ' + challenge.creator?.username
+                  : 'Créez en un dès maintenant !'}
+              </p>
+            </div>
           </div>
         </div>
       )}

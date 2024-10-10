7d388935b5671f568c6e1736864097987a18d831
Nico <nicobdx@icloud.com>
2024-10-10 20:41:21 +0200

    remove confirm mail msg

--- components/signup/SignupForm.tsx
+++ components/signup/SignupForm.tsx
@@ -68,9 +68,7 @@ export default function SignUpForm() {
       if (error) {
         toast.error(error);
       } else {
-        toast.success(
-          'Un email de confirmation vous a été envoyé (à faire avec resend))',
-        );
+        toast.success('Inscription réussie !');
         router.push('/connexion');
       }
     } catch (error) {
